401e4371ef28bc3530a43e69513fd1c6b4967e49
William Rizzi da Silva <silva.william@cwi.com.br>
2024-06-10 08:00:37 -0300

    feat: add cors options

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function initServer() {
+  const configService = new ConfigService();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: configService.get<string>('REQUEST_ORIGIN') });
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
