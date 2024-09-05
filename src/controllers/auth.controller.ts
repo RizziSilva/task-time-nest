c2f82eaf2538502a7c2fca4396eaa04354ec1d01
RizziSilva <rizzi.william@hotmail.com>
2024-09-05 08:01:20 -0300

    feat: add cookie parser to handle cookies on api

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
 import { RequestLoginUser } from '@decorators';
 import { AuthLoginResponseDto, LoginResponseDto } from '@dtos';
 import { AuthService } from '@services';
@@ -11,7 +12,38 @@ export class AuthController {
   @UseGuards(UserAuthGuard)
   @HttpCode(HttpStatus.OK)
   @Post('/login')
-  async login(@RequestLoginUser() user: AuthLoginResponseDto): Promise<LoginResponseDto> {
-    return await this.authService.login(user);
+  async login(
+    @Res({ passthrough: true }) res: Response,
+    @RequestLoginUser() user: AuthLoginResponseDto,
+  ): Promise<LoginResponseDto> {
+    const response: LoginResponseDto = await this.authService.login(user);
+
+    res.cookie('access_token', response.token.access_token, {
+      httpOnly: true,
+      path: '/',
+      sameSite: true,
+    });
+
+    res.cookie('refresh_token', response.token.refresh_token, {
+      httpOnly: true,
+      path: '/',
+      sameSite: true,
+    });
+
+    return response;
   }
 }
+// const response: LoginResponseDto = await this.authService.login(user);
+
+//     // res.cookie('access_token', response.token.access_token, {
+//     //   httpOnly: true,
+//     //   path: '/',
+//     //   sameSite: true,
+//     // });
+//     // res.cookie('refresh_token', response.token.refresh_token, {
+//     //   httpOnly: true,
+//     //   path: '/',
+//     //   sameSite: true,
+//     // });
+
+//     return response;
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/exception.filter';
-import { ConfigService } from '@nestjs/config';
 
 async function initServer() {
   const configService = new ConfigService();
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: configService.get<string>('REQUEST_ORIGIN') });
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(cookieParser());
 
   await app.listen(3000);
 }
